1af9dd4195a74aeab4daf28a085420e9b9d6681f
khuzamas <khuzamshu@gmail.com>
2023-05-20 15:43:19 +0300

    welcome page edited

--- OptionsPage.php
+++ OptionsPage.php
@@ -10,6 +10,9 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <style>
+        /* * {
+            font-family: ;
+        } */
         body{
             margin: 0;
             padding: 0;
